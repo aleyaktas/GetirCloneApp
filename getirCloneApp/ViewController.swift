a81cf087e7e37b8d653685c8e11ddbdb59234103
Aleyna Aktaş <aleynaaktas627@gmail.com>
2023-08-08 00:14:13 +0300

    Initial Commit

---
+++ getirCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  getirCloneApp
+//
+//  Created by Aleyna Aktaş on 8.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
