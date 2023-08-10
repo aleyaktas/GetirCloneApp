9ecd1346a6eaac537bdb9f500e3405efb9b4a70e
Aleyna Aktaş <aleynaaktas627@gmail.com>
2023-08-10 16:23:18 +0300

    Add navigation bar and edit tab bar style

--- getirCloneApp/AppDelegate.swift
+++ getirCloneApp/AppDelegate.swift
@@ -11,9 +11,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.rootViewController = CustomTabBar()
+//        window?.makeKeyAndVisible()
+//        
         return true
     }
 
@@ -30,7 +35,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
--- getirCloneApp/Main Page/MainPageVC.swift
+++ getirCloneApp/Main Page/MainPageVC.swift
@@ -11,9 +11,36 @@ class MainPageVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
+        let apperance = UITabBarAppearance()
+        
+        apperance.backgroundColor = .white
+        
+        changeColor(itemApperance: apperance.stackedLayoutAppearance)
+        changeColor(itemApperance: apperance.inlineLayoutAppearance)
+        changeColor(itemApperance: apperance.compactInlineLayoutAppearance)
+        
+        tabBarController?.tabBar.standardAppearance = apperance
+        tabBarController?.tabBar.scrollEdgeAppearance = apperance
+        }
+      
+    func changeColor(itemApperance: UITabBarItemAppearance) {
+        itemApperance.selected.iconColor = UIColor(named: "purple")
     }
+    
+    private func setupNavigationBar() {
+        navigationItem.title = ""
 
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        imageView.contentMode = .scaleAspectFit
 
+        navigationItem.titleView = imageView
+
+        let imageSize = CGSize(width: 70, height: 70)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.widthAnchor.constraint(equalToConstant: imageSize.width).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: imageSize.height).isActive = true
+    }
 }
+
 
