08bfaa5b28352c195633de92fa4f9d1455b8e2ef
Aleyna Aktaş <aleynaaktas627@gmail.com>
2023-08-11 13:08:17 +0300

    Add address bar to Main Page

--- getirCloneApp/Main Page/MainPageVC.swift
+++ getirCloneApp/Main Page/MainPageVC.swift
@@ -9,20 +9,17 @@ import UIKit
 
 class MainPageVC: UIViewController {
 
+    @IBOutlet weak var addressButtonLabel: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
-        let apperance = UITabBarAppearance()
-        
-        apperance.backgroundColor = .white
-        
-        changeColor(itemApperance: apperance.stackedLayoutAppearance)
-        changeColor(itemApperance: apperance.inlineLayoutAppearance)
-        changeColor(itemApperance: apperance.compactInlineLayoutAppearance)
-        
-        tabBarController?.tabBar.standardAppearance = apperance
-        tabBarController?.tabBar.scrollEdgeAppearance = apperance
-        }
+        setupTabBar()
+        editAdressStyle()
+    }
+    
+    
       
     func changeColor(itemApperance: UITabBarItemAppearance) {
         itemApperance.selected.iconColor = UIColor(named: "purple")
@@ -40,6 +37,42 @@ class MainPageVC: UIViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.widthAnchor.constraint(equalToConstant: imageSize.width).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: imageSize.height).isActive = true
+    }
+    
+    private func setupTabBar() {
+        let apperance = UITabBarAppearance()
+        
+        apperance.backgroundColor = .white
+        
+        changeColor(itemApperance: apperance.stackedLayoutAppearance)
+        changeColor(itemApperance: apperance.inlineLayoutAppearance)
+        changeColor(itemApperance: apperance.compactInlineLayoutAppearance)
+        
+        tabBarController?.tabBar.standardAppearance = apperance
+        tabBarController?.tabBar.scrollEdgeAppearance = apperance
+    }
+    
+    private func editAdressStyle() {
+        let fullAddress = "Ev, Atatürk, 1938.Sk, no: 14, Merkez/Ankara"
+         
+        if let commaRange = fullAddress.range(of: ",") {
+            let firstPart = String(fullAddress[..<commaRange.lowerBound])
+            
+            let attributedText = NSMutableAttributedString(string: fullAddress)
+            
+            attributedText.removeAttribute(.foregroundColor, range: NSRange(location: 0, length: attributedText.length))
+            attributedText.removeAttribute(.font, range: NSRange(location: 0, length: attributedText.length))
+            
+            let nsString = fullAddress as NSString
+            
+            let rangeOfFirstPart = NSRange(location: 0, length: nsString.range(of: firstPart).upperBound)
+            
+            attributedText.addAttribute(.foregroundColor, value: UIColor(named: "purple") ?? UIColor.purple, range: rangeOfFirstPart)
+            attributedText.addAttribute(.font, value: UIFont.systemFont(ofSize: 12), range: NSRange(location: 0, length: attributedText.length))
+            attributedText.addAttribute(.font, value: UIFont.boldSystemFont(ofSize: 16), range: rangeOfFirstPart)
+            
+            addressButtonLabel.setAttributedTitle(attributedText, for: .normal)
+        }
     }
 }
 
